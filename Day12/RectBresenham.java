4944d50fd085ac1742836984050d6dc1ca2017e4
Thilini Kavindya <thilinikavindya678@gmail.com>
2026-08-16 11:50:07 +0530

    Add files via upload

---
+++ Day12/RectBresenham.java
@@ -0,0 +1,60 @@
+import javax.swing.*; 
+import java.awt.*; 
+
+public class RectBresenham extends JFrame 
+{
+	public RectBresenham() 
+	{
+		setTitle("RectBresenham");
+		setSize(600,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		
+		drawbresenham(g,100,100,200,100);
+		drawbresenham(g,100,100,100,200);
+		drawbresenham(g,100,200,200,200);
+		drawbresenham(g,200,200,200,100);
+	}
+	
+	public void drawbresenham(Graphics g,int x1,int y1,int x2,int y2)
+	{		
+		int dx=Math.abs(x2-x1);
+		int dy=Math.abs(y2-y1);
+		
+		int incX=(x1<x2) ? 1:-1;
+		int incY=(y1<y2) ? 1:-1;
+		
+		int err=dx-dy;
+		
+		while(true)
+		{
+			g.drawRect(x1,y1,2,2);
+			if(x1==x2 && y1==y2)
+				break;
+			
+			int e2=2*err;
+			
+			if(e2>-dy)
+			{
+				err=err-dy;
+				x1+=incX;
+			}
+			
+			if(e2<dx)
+			{
+				err=err+dx;
+				y1+=incY;
+			}
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		new RectBresenham();
+	}
+}
